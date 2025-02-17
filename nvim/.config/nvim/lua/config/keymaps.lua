ebbca07f36007460d552d9a1419fe4dd3ca11ec9
Digvijay Mahapatra <thediggu@gmail.com>
2025-02-17 23:42:20 +0530

    feat: Add codecompanion

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -11,6 +11,7 @@ vim.keymap.set(
 vim.keymap.set({ "n", "v" }, "<leader>]", ":Gen<CR>")
 
 -- Diffview
+vim.keymap.set("n", "<leader>gD", "", { noremap = true, desc = "GitAdvanced" })
 vim.keymap.set("n", "<leader>gDo", ":DiffviewOpen<CR>", { noremap = true, desc = "Open Diffview" })
 vim.keymap.set("n", "<leader>gDl", ":DiffviewFileHistory<CR>", { noremap = true, silent = true, desc = "View history" })
 vim.keymap.set(
@@ -18,4 +19,25 @@ vim.keymap.set(
   "<leader>gDf",
   ":DiffviewFileHistory %<CR>",
   { noremap = true, silent = true, desc = "View file history" }
+)
+
+-- CodeCompanion
+vim.keymap.set({ "n", "v" }, "<leader>a", "", { noremap = true, desc = "CodeCompanion" })
+vim.keymap.set(
+  { "n", "v" },
+  "<leader>aa",
+  ":CodeCompanionActions<CR>",
+  { noremap = true, desc = "CodeCompanion actions" }
+)
+vim.keymap.set(
+  { "n", "v" },
+  "<leader>ac",
+  ":CodeCompanionChat Toggle<CR>",
+  { noremap = true, desc = "CodeCompanion chat" }
+)
+vim.keymap.set(
+  { "n", "v" },
+  "<leader>ad",
+  ":CodeCompanionChat Add<CR>",
+  { noremap = true, desc = "CodeCompanion add to chat" }
 )
