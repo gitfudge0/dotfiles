bc33bebcb7b5f3640201b96dce31cc74a56081c4
Digvijay Mahapatra <thediggu@gmail.com>
2025-02-18 00:12:40 +0530

    chore: Add more prompts to codecompanion

--- nvim/.config/nvim/lua/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -1,3 +1,5 @@
+local fmt = string.format
+
 return {
   "olimorris/codecompanion.nvim",
   dependencies = {
@@ -31,6 +33,16 @@ return {
       },
       inline = {
         layout = "vertical", -- vertical|horizontal|buffer
+        adapter = "qwen",
+        prompts = {
+          -- The prompt to send to the LLM when a user initiates the inline strategy and it needs to convert to a chat
+          inline_to_chat = function(context)
+            return fmt(
+              [[I want you to act as an expert and senior developer in the %s language. I will ask you questions, perhaps giving you code examples, and I want you to advise me with explanations and code where necessary.]],
+              context.filetype
+            )
+          end,
+        },
         keymaps = {
           accept_change = {
             modes = { n = "ga" },
@@ -91,6 +103,40 @@ return {
         layout = "vertical", -- vertical|horizontal split for default provider
         opts = { "internal", "filler", "closeoff", "algorithm:patience", "followwrap", "linematch:120" },
         provider = "mini_diff", -- default|mini_diff
+      },
+    },
+    prompt_library = {
+      ["Code Review"] = {
+        strategy = "chat",
+        description = "Get code review from an LLM",
+        opts = {
+          modes = { "v" },
+          short_name = "review",
+          auto_submit = true,
+          stop_context_insertion = true,
+          user_prompt = false,
+        },
+        prompts = {
+          {
+            role = "system",
+            content = function(context)
+              return "I want you to act as a senior "
+                .. context.filetype
+                .. " developer. I want you to review the code and give me suggestions that improve the code quality."
+            end,
+          },
+          {
+            role = "user",
+            content = function(context)
+              local text = require("codecompanion.helpers.actions").get_code(context.start_line, context.end_line)
+
+              return "I have the following code:\n\n```" .. context.filetype .. "\n" .. text .. "\n```\n\n"
+            end,
+            opts = {
+              contains_code = true,
+            },
+          },
+        },
       },
     },
   },
